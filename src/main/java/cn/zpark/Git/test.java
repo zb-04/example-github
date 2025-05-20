74d5e52fa73889b19a9044de050add12683f6a0e
zb-04 <zb041010@126.com>
2025-05-20 09:17:44 +0800

    Update test.java
    远程仓库提交

--- src/main/java/cn/zpark/Git/test.java
+++ src/main/java/cn/zpark/Git/test.java
@@ -3,6 +3,7 @@ package cn.zpark.Git;
 public class test {
     public static void main(String[] args) {
         System.out.println("zb");
-        System.out.println("成功")
+        System.out.println("成功");
+        System.out.println("agin成功");
     }
 }
